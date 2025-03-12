a7346524a12cb4315b92120f6201c4e57e5b8ea3
KhangChau12 <phuckhangtdn@gmail.com>
2025-03-13 00:14:20 +0700

    2

--- server/config/db.js
+++ server/config/db.js
@@ -1,10 +1,19 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
+const clientOptions = { 
+  serverApi: { 
+    version: '1', 
+    strict: true, 
+    deprecationErrors: true 
+  } 
+};
+
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
+    await mongoose.connect(process.env.MONGO_URI, clientOptions);
+    await mongoose.connection.db.admin().command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } catch (error) {
     console.error(`Error: ${error.message}`);
     process.exit(1);
